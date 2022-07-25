100002efd9319db83fa371ebd939c1c1815cc5f7
patipan-patisampita <patipan.patisampita@gmail.com>
2022-07-25 15:45:40 +0700

    update

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+const hostname = 'localhost'
+
+//GET: http://localhost:5000/
+app.get('/',(req,res)=>{
+    res.send('<h1>Express App</h1><hr color="red" />')
+})
+
+//GET: http://localhost:5000/contact
+app.get('/contact',(req,res)=>{
+    res.send('<h1>Contact Page</h1><hr color="red" />')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://${hostname}:${PORT}`)
+})
